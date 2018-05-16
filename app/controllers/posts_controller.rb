eb19f69252ab0854cb3ae192b64e3b2c57d5d106
Elle Faraday <faraday.elle@gmail.com>
2018-05-16 20:41:05 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,7 +8,6 @@ class PostsController < ApplicationController
 	end
 
 	def new
-
 	end
 
 	def create
@@ -16,5 +15,6 @@ class PostsController < ApplicationController
 	  post.title = params[:title]
 	  post.description = params[:description]
 	  post.save
+		redirect_to posts_path
 	end
 end
